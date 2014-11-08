6271b310121e15d3bc9701ca86a1e2cef65ecb52
SGospodinov <sani9706@gmail.com>
2014-11-08 10:46:31 +0200

    stuff

---
+++ ThePatka/src/com/yellow_patka/thepatka/PlayActivity.java
@@ -0,0 +1,13 @@
+package com.yellow_patka.thepatka;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play);
+	}
+}
