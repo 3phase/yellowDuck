b958c9d9dad1fb5fe5bf2f5f8358579af1ddf302
3phase <pbozhinov@yahoo.com>
2014-11-08 12:20:30 +0200

    hahha

---
+++ ThePatka/src/com/yellow_patka/thepatka/SelectActivity.java
@@ -0,0 +1,16 @@
+package com.yellow_patka.thepatka;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SelectActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select);
+	}
+	
+}
