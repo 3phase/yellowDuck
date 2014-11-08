3dc21d0d136c3be321ac7a12a8e04d83d6f16663
SGospodinov <sani9706@gmail.com>
2014-11-08 11:52:17 +0200

    otherStuff

--- ThePatka/src/com/yellow_patka/thepatka/PlayActivity.java
+++ ThePatka/src/com/yellow_patka/thepatka/PlayActivity.java
@@ -1,13 +1,30 @@
 package com.yellow_patka.thepatka;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class PlayActivity extends Activity {
+public class PlayActivity extends Activity implements OnClickListener{
 
+	Button back;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_play);
+		back = (Button) findViewById(R.id.backBtn);
+		back.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == back.getId()){
+			Intent backIntent = new Intent(this, MainActivity.class);
+			startActivity(backIntent);
+		}
+		
 	}
 }
